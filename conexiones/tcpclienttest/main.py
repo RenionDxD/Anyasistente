7a62a22b16c9a083e16a627740f2003d99f97c26
Claudio Cataño <layox.5698@gmail.com>
2022-07-13 00:04:42 -0500

    sounddevice replaces SpeechRecognition.listen cos pyaudio

--- conexiones/tcpclienttest/main.py
+++ conexiones/tcpclienttest/main.py
@@ -3,13 +3,13 @@ import funciones_basicas.Funciones_voz as Fv
 
 def tcpclient(orden,modulo):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 10000)
+    server_address = ('192.168.0.16', 50001)
     print('connecting to ' + server_address[0])
     sock.connect(server_address)
 
     try:
         print(orden,modulo)
-        message = f'action|open|{orden}|{modulo}'.encode('utf8')
+        message = f'action|{orden}|{modulo}'.encode('utf8')
 
         sock.sendall(message)
 
--- funciones_basicas/Funciones_voz.py
+++ funciones_basicas/Funciones_voz.py
@@ -3,8 +3,7 @@ from time import sleep
 import speech_recognition as sr
 import subprocess as sub
 import jsons.des_cod as frase
-
-
+from . import Record
 
 nombre = 'juno'
 r = sr.Recognizer()
@@ -17,53 +16,58 @@ def habla(rec):
     command = f'gtts-cli -l es -t com "{rec}" | play -q -t mp3 - tempo 1.3 '
     sub.run([command], shell=True)
 
+
 def MensajesVoz():
     try:
-        with sr.Microphone() as source:
-            habla("Escuchando...2")
-            r.adjust_for_ambient_noise(source, 1)
-            audio = s.listen(source)
-            mes = s.recognize_google(audio, language="es")
-            mes = mes.lower()
-    except: 
-            habla('Perdona no pude entenderte')
-            
-    return mes    
+        # with sr.Microphone() as source:
+        #    habla("Escuchando...2")
+        #    r.adjust_for_ambient_noise(source, 1)
+        #    audio = s.listen(source)
+        mes = Record.listen()
+        mes = s.recognize_google(mes, language="es")
+        mes = mes.lower()
+    except:
+        habla('Perdona no pude entenderte')
+
+    return mes
+
 
 def listen():
     rec = ""
     try:
-        with sr.Microphone() as source:
-                habla(frase.palabras("te_escucho"))
-                r.adjust_for_ambient_noise(source, 1)
-                audio = r.listen(source)
-                rec = r.recognize_google(audio, language="es")
-                rec = rec.lower()
-                if nombre in rec:
-                    rec = rec.replace(nombre, '')
-                    print(rec)
+        # with sr.Microphone() as source:
+        #    habla(frase.palabras("te_escucho"))
+        #    r.adjust_for_ambient_noise(source, 1)
+        #    audio = r.listen(source)
+        rec = Record.listen()
+        rec = r.recognize_google(rec, language="es")
+        rec = rec.lower()
+        if nombre in rec:
+            rec = rec.replace(nombre, '')
+            print(rec)
     except sr.UnknownValueError:
-         print("no se entendio")
-         habla(frase.palabras("no_entiendo"))
-    except: 
-          habla(frase.palabras("no_entiendo"))
+        print("no se entendio")
+        habla(frase.palabras("no_entiendo"))
+    except:
+        habla(frase.palabras("no_entiendo"))
     return rec
 
 
 def Atencion():
     llamado = ""
     try:
-        with sr.Microphone() as source:
-                print("escuchando....")
-                r.adjust_for_ambient_noise(source, 1)
-                audio = r.listen(source)
-                llamado = r.recognize_google(audio, language="es")
-                llamado = llamado.lower()
-                if nombre in llamado:
-                    llamado = llamado.replace(nombre, '')
-                    print(llamado)
+        # with sr.Microphone() as source:
+        #        print("escuchando....")
+        #        r.adjust_for_ambient_noise(source, 1)
+        #        audio = r.listen(source)
+        llamado = Record.listen()
+        llamado = r.recognize_google(llamado, language="es")
+        llamado = llamado.lower()
+        if nombre in llamado:
+            llamado = llamado.replace(nombre, '')
+            print(llamado)
     except sr.UnknownValueError:
-         print("No se entendio lo que dijiste")
-    except: 
-          print("B")
+        print("No se entendio lo que dijiste")
+    except:
+        print("B")
     return llamado
